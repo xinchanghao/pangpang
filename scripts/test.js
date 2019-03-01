87487771244e39223b78395adc6ee8c54c2fc94a
ChhXin <chh.xin@gmail.com>
2019-03-01 18:59:26 +0800

    modify:test jest

--- scripts/test.js
+++ scripts/test.js
@@ -5,9 +5,7 @@ process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+
 process.on('unhandledRejection', err => {
   throw err;
 });
@@ -23,5 +21,6 @@ if (!process.env.CI && argv.indexOf('--coverage') < 0) {
   argv.push('--watch');
 }
 
+// argv.push('--watch');
 
 jest.run(argv);
